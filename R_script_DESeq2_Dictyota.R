9aff2ec86744ea2a0db3a883d91373ff82374f75
Gonçalo Faria <73292974+DeFariaJ@users.noreply.github.com>
2021-04-01 15:07:31 +0200

    Update R_script_DESeq2_Dictyota.R

--- R_script_DESeq2_Dictyota.R
+++ R_script_DESeq2_Dictyota.R
@@ -75,17 +75,56 @@ results_df <- as.data.frame(result_table)
 
 #######FILTERING################
 
-## this would throw away rows (genes) with baseline NA or other NA
-#complete.cases(results_df) #complete.cases returns TRUE for columns data have all data (no NAs)
-#filter_df <- results_df[complete.cases(results_df),]
+b = subset(results_df, padj<.05 & abs(log2FoldChange)>1)#select the desired p value and FC
 
 
-# so we go directly to p-value and foldChange filtering
+## Filter rows (genes) with all columns == NA
+complete.cases(results_df) #complete.cases returns TRUE for columns data have all data (no NAs)
+filter_df1 <- results_df[complete.cases(results_df),]
+
 # adj p-value <0.05
-results_df$padj <0.05
-filter_df1 <- results_df[results_df$padj <0.05,]
+filter_df1$padj <0.05
+filter_df2 <- filter_df1[filter_df1$padj <0.05,]
 
 #p-value <0.05 is not enough, we also want to have a large effect size
 #log2FoldChange >= 1 <= -1, we will filter it on magnitude and not sign, regardless of direction
-abs(filter_df1$log2FoldChange) > 1
-filter_df2 <- filter_df1[abs(filter_df1$log2FoldChange) > 1,]
+abs(filter_df2$log2FoldChange) > 1
+ 
+ 
+ 
+filter_df3 <- filter_df2[abs(filter_df2$log2FoldChange) > 1,]
+
+
+###                                                                 ###
+####################### VISULALISATION ################################
+
+#MA plot
+
+plotMA(result_table) #takes as input original table produced by DESeq2 (not data.frame) #CAN CHANGE TO 0.05
+# the aim here is to produce a comparison of the mean normalized counts (baseMean) and fold change
+#adjusted p-value here is <0.1 and I am not sure how to change this
+#so a lot of the blue dots are not significant 
+# still, the higher the mean counts increases the more likely we are to get DGE genes
+# nothing unusual about the data which is good
+
+
+######## VOLCANO PLOT ############
+#for all the genes we have analysied, we need the fold change and the adj p-value
+
+#creating a new column on filter_table_df1 which satisfies both conditions 
+filter_df1$test <- abs(filter_df1$log2FoldChange) > 1 & filter_df1$padj <0.05
+
+g <- ggplot(filter_df1, aes(x=log2FoldChange, y= -log10(padj))) +
+         geom_point(aes(colour=test), size=1, alpha=0.3) +
+         scale_colour_manual(values = c("black", "red")) +
+         geom_vline(xintercept = 1, colour= "black", linetype=2) + 
+         geom_vline(xintercept = -1, colour="black", linetype=2) +
+         geom_hline(yintercept = -log10(0.05), colour = "black", linetype = 2) 
+         
+ggplotly(g) #assign the plot above to g and make an interactive plot
+        
+         
+with(results_df, plot(log2FoldChange, -log10(pvalue), pch=20, main="Dictyota Female vs Male GA"))
+with(subset(results_df, padj<.05 & abs(log2FoldChange)>1), points(log2FoldChange, -log10(pvalue), pch=20, col="grey"))
+with(subset(results_df, padj<.05 & abs(log2FoldChange)>2), points(log2FoldChange, -log10(pvalue), pch=20, col="orange"))
+with(subset(results_df, padj<.05 & abs(log2FoldChange)>4), points(log2FoldChange, -log10(pvalue), pch=20, col="darkgreen"))
