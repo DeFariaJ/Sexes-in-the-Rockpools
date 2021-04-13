e19e1e4937b9e9e39b177f3b6c18ef5eeb292d3d
Gonçalo Faria <73292974+DeFariaJ@users.noreply.github.com>
2021-04-13 16:52:48 +0200

    Update FBGs_vs_MBGs_Dictyota.R

--- FBGs_vs_MBGs_Dictyota.R
+++ FBGs_vs_MBGs_Dictyota.R
@@ -1,7 +1,6 @@
 ################################################################################################################
 #trying to further analysis SBGE number in males vs females
-#not sure if this is correct but here we go
-
+#talk to Aga, not sure if this is correct but here we go
 
 #create a female_up df which is a subset of binary heat_map females > males
 Female_Up <- subset(binary_heatMap2[binary_heatMap2$Females>binary_heatMap2$Males,])
@@ -14,7 +13,7 @@ Male_Up <- subset(binary_heatMap2[binary_heatMap2$Females<binary_heatMap2$Males,
 ####################
 
 #extract row names (genes) from filter_DF1 with FC > 2
-fold_2_genes <- row.names(filter_df1[filter_df1$padj<.05 & abs(filter_df1$log2FoldChange) >2,])
+fold_2_genes <- row.names(filter_df1[filter_df1$padj<.05 & abs(filter_df1$log2FoldChange) >1,])
 #logical matrix, genes which are both present in Female_up and filter_DF1
 logical_vector <-(rownames(Female_Up) %in% (fold_2_genes))
 #count number of TRUES which (I think, hope, presume) = number of genes FBG FC >2
@@ -25,7 +24,7 @@ sum(logical_vector, na.rm = TRUE) # best way to count TRUE values
 #####################
 
 #extract row names (genes) from filter_DF1 with FC > 4
-fold_4_genes <- row.names(filter_df1[filter_df1$padj<.05 & abs(filter_df1$log2FoldChange) >4,])
+fold_4_genes <- row.names(filter_df1[filter_df1$padj<.05 & abs(filter_df1$log2FoldChange) >2,])
 #logical matrix, genes which are both present in Female_up and filter_DF1
 logical_vector <-(rownames(Female_Up) %in% (fold_4_genes))
 #count number of TRUES which (I think, hope, presume) = number of genes FBG FC >4
@@ -36,7 +35,7 @@ sum(logical_vector, na.rm = TRUE) # best way to count TRUE values
 #####################
 
 #extract row names (genes) from filter_DF1 with FC > 4
-fold_6_genes <- row.names(filter_df1[filter_df1$padj<.05 & abs(filter_df1$log2FoldChange) >6,])
+fold_6_genes <- row.names(filter_df1[filter_df1$padj<.05 & abs(filter_df1$log2FoldChange) >2.585,])
 #logical matrix, genes which are both present in Female_up and filter_DF1
 logical_vector <-(rownames(Female_Up) %in% (fold_6_genes))
 #count number of TRUES which (I think, hope, presume) = number of genes FBG FC >4
@@ -72,6 +71,4 @@ sum(logical_vector, na.rm = TRUE) # best way to count TRUE values
 
 
 #Sanity check
-sanity_check <- row.names(filter_df3[filter_df3$padj<.05 & abs(filter_df3$log2FoldChange) <=2,])
-
-#####this is fucking correct!!!! I think....
+sanity_check <- row.names(filter_df1[filter_df1$padj>.05 & abs(filter_df1$log2FoldChange) <=1,])
